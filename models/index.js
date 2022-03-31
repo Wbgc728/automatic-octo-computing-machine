cf487a0028f3b4508292d447acfac4ce502231e1
wbgc728 <wbgc728@gmail.com>
2022-03-30 23:14:23 -0600

    Updated model files

--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catgory_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_tags'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+TimeRanges.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag,
+    unique:false
+  },
+  
+})
 
 module.exports = {
   Product,
