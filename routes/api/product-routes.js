ab52a606930eaec36034c74af110f1dffa084764
wbgc728 <wbgc728@gmail.com>
2022-04-03 20:27:33 -0600

    Fixed all GETs

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,14 +22,14 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const ProductData = await Product.findByPk(req.params.id, {
+    const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category },{model: Tag, through: ProductTag, as: 'products_tags'}]
     });
-    if (!categoryData) {
+    if (!productData) {
       res.status(404).json({ message: 'No category found' });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -113,7 +113,7 @@ router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try{
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category },{model: Tag, through: ProductTag, as: 'product_tags'}]
+      include: [{ model: Category },{model: Tag, through: ProductTag, as: 'products_tags'}]
     });
     if (!productData) {
       res.status(404).json({message: 'No product found'});
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag, as: 'tags_products' }]
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found' });
